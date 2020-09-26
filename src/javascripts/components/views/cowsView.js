a8985aa759063f8e8ff7fe4b954f2d1ccd4c513b
josephtmartin <goldglovejo13@gmail.com>
2020-09-26 14:30:18 -0500

    updates from saturday class

--- src/javascripts/components/views/cowsView.js
+++ src/javascripts/components/views/cowsView.js
@@ -1,16 +1,17 @@
-import cowData from '../../helpers/data/cowData';
+import mergedData from '../../helpers/data/mergedData';
 import card from '../cards/cowCard';
 
 const cowsView = () => {
-  cowData.getCows().then((response) => {
-    if (response.length) {
-      response.forEach((item) => {
-        $('#app').append(card.cowMaker(item));
-      });
-    } else {
-      $('#app').html('<h1>No Cows!</h1>');
-    }
-  });
+  mergedData.getDataForCowsView()
+    .then((response) => {
+      if (response.length) {
+        response.forEach((item) => {
+          $('#app').append(card.cowMaker(item));
+        });
+      } else {
+        $('#app').html('<h1>No Cows!</h1>');
+      }
+    });
 };
 
 export default { cowsView };
--- src/javascripts/helpers/data/farmerData.js
+++ src/javascripts/helpers/data/farmerData.js
@@ -81,4 +81,9 @@ const deleteFarmer = (farmerUid) => {
     });
 };
 
-export default { setCurrentFarmer, getAllFarmers, deleteFarmer };
+export default {
+  setCurrentFarmer,
+  getAllFarmers,
+  deleteFarmer,
+  getSingleFarmer
+};
